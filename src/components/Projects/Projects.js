c6aad56f7129805ddee540cb4e0c41237c5a71ca
The-SaqlianNawaz <pacsaqiirr@gmail.com>
2023-03-25 19:37:15 +0500

    ready

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,6 @@ import memory from "../../Assets/Projects/memory.png";
 import Airbnb from "../../Assets/Projects/airbn.png";
 import calculator from "../../Assets/Projects/calculator.png";
 import tenzies from "../../Assets/Projects/tenzies.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import bluespace from "../../Assets/Projects/bluespace.png";
 
 function Projects() {
@@ -37,20 +36,9 @@ function Projects() {
               imgPath={calculator}
               isBlog={false}
               title="Calculator"
-              description="This calculator supports basic arithmetic operations such as addition, subtraction, multiplication, and division. It also supports decimal numbers and has a clear button to reset the calculator."
+              description="This calculator supports basic arithmetic operations on operands such as addition, subtraction, multiplication, and division. It also supports decimal numbers and has a clear button to reset the calculator. A simple Calculator but beautiful GUI, provide all the basic operations. It is made with React.js."
               ghLink="https://github.com/The-SaqlainNawaz/Calculator"
               demoLink="https://the-saqlainnawaz.github.io/Calculator/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Airbnb}
-              isBlog={false}
-              title="Airbnb clone"
-              description=" the Airbnb Clone React is a front-end only project that demonstrates how React can be used to create a functional and attractive user interface for a web application. However, it does not include any backend functionality to handle user data or booking management.the Airbnb Clone React repository provides an interface for a clone of the Airbnb website built using React, but without any backend functionality."
-              ghLink="https://github.com/The-SaqlainNawaz/Airbnb-Clone-React"
-              demoLink="https://github.com/The-SaqlainNawaz/Airbnb-Clone-React"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -66,6 +54,17 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={Airbnb}
+              isBlog={false}
+              title="Airbnb clone"
+              description="The Airbnb Clone React is a front-end only project that demonstrates how React can be used to create a functional and attractive user interface for a web application. However, it does not include any backend functionality."
+              ghLink="https://github.com/The-SaqlainNawaz/Airbnb-Clone-React"
+              demoLink="https://github.com/The-SaqlainNawaz/Airbnb-Clone-React"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={bluespace}
               isBlog={false}
               title="BlueSpace"
@@ -74,18 +73,6 @@ function Projects() {
               demoLink="https://github.com/The-SaqlainNawaz/Social_Media_website"
             />
           </Col>
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Airbnb}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
